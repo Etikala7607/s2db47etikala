0d7c601932cdf29f8ea2a72df95b30653c7adb38
Sravan Kumar <75154116+Etikala7607@users.noreply.github.com>
2022-04-15 11:19:18 -0500

    Update devil.js

--- controllers/devil.js
+++ controllers/devil.js
@@ -12,21 +12,53 @@ exports.devil_list = async function(req, res) {
 }; 
  
 // for a specific Costume. 
-exports.devil_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id); 
-}; 
+exports.devil_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await devil.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+};
  
 // Handle Costume create on POST. 
-exports.devil_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Costume create POST'); 
+exports.devil_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new devil(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.devil_name = req.body.devil_name;
+    document.version = req.body.version;
+    document.type = req.body.type; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
- 
 // Handle Costume delete form on DELETE. 
 exports.devil_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id); 
+    res.send('NOT IMPLEMENTED: devil delete DELETE ' + req.params.id); 
 }; 
  
 // Handle Costume update form on PUT. 
 exports.devil_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Costume update PUT' + req.params.id); 
+    res.send('NOT IMPLEMENTED: devil update PUT' + req.params.id); 
+}; 
+exports.devil_view_all_Page = async function(req, res) { 
+    try{ 
+        thedevils = await devil.find(); 
+        res.render('devil', { title: 'devil Search Results', results: thedevils }); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
