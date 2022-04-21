fd231f5610ac01b487b79434d4af65e8229606c0
Sravan Kumar <75154116+Etikala7607@users.noreply.github.com>
2022-04-21 15:29:26 -0500

    Update devil.js

--- routes/devil.js
+++ routes/devil.js
@@ -5,4 +5,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', devil_controlers.devil_view_all_Page ); 
 
+router.get('/detail', devil_controlers.devil_view_one_Page);
+router.get('/create', devil_controlers.devil_create_Page); 
+router.get('/update', devil_controlers.devil_update_Page);
+router.get('/delete', devil_controlers.devil_delete_Page);
+
 module.exports = router;
